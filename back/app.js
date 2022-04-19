f1a00c57f0d9a35851766ccdc601ccbb45107432
Lewogona <lewogona@gmail.com>
2022-04-19 16:13:09 +0200

    user

--- back/app.js
+++ back/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const userRoutes = require('./routes/user');
+
 const app = express();
 
 mongoose.connect('mongodb+srv://Lewogona:gaxUK2VQF9w81Eb6@cluster0.pi6d3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
@@ -11,9 +13,17 @@ mongoose.connect('mongodb+srv://Lewogona:gaxUK2VQF9w81Eb6@cluster0.pi6d3.mongodb
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
