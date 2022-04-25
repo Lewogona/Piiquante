b43807c0fc46de93aac9f32239996be1e7ac495a
Lewogona <lewogona@gmail.com>
2022-04-25 15:50:31 +0200

    added sauces

--- back/app.js
+++ back/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
-const userRoutes = require('./routes/user');
+const sauceRoutes = require("./routes/sauce");
+const userRoutes = require("./routes/user");
 
 const app = express();
 
@@ -20,10 +22,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
+app.use("/api/sauces", sauceRoutes);
 app.use("/api/auth", userRoutes);
 
 module.exports = app;
