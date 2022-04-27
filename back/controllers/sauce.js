1fdbc4db9e800b3a1a42e1b39df4f2ba5675f59f
Lewogona <lewogona@gmail.com>
2022-04-27 13:29:55 +0200

    improved auth middleware for delete

--- back/controllers/sauce.js
+++ back/controllers/sauce.js
@@ -40,7 +40,14 @@ exports.modifySauce = (req, res, next) => {
 
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
-        .then(sauce => {
+        .then((sauce) => {
+            if (!sauce) {
+                return res.status(404).json({ error: "Sauce non trouvée !" })
+            };
+            if (sauce.userId !== req.auth.userId) {
+                return res.status(401).json({ error: new Error("Requête non autorisée !") })
+            };
+            
             const filename = sauce.imageUrl.split("/images/")[1];
             fs.unlink(`images/${filename}`, () => {
                 Sauce.deleteOne({ _id: req.params.id })
