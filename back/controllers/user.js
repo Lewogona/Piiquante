27a8a9a9c82842e4e7d85aca5cc9ae28062e5edd
Lewogona <lewogona@gmail.com>
2022-05-16 21:01:06 +0200

    cleaned

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -35,11 +35,7 @@ exports.login = (req, res, next) => {
         }
         res.status(200).json({
             userId: user._id,
-            token: jwt.sign(
-            { userId: user._id },
-            process.env.JWT_SECRET,
-            { expiresIn: '24h' }
-            )
+            token: jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '24h' })
         });
         })
         .catch(error => res.status(500).json({ error }));
